5191c0b3c9122a6c780652b1c1053e9095c3b49e
unknown <marlon_tancredo@hotmail.com>
2023-08-26 20:16:56 +0100

    feat: implementing handleFeedBack function

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const feedbacks = {
+  red: "Should be longer",
+  orange: "Pretty good",
+  green: "Grrrreat!",
+};
+
 const increaseProgressBar = (inputValue) => {
   if (inputValue.length > 20) {
     return "100%";
@@ -16,12 +22,27 @@ const handleProgressBarColor = (inputValue) => {
   return "black";
 };
 
+const handleFeedback = () => {
+  const internProgress = document.getElementById("intern-progress");
+  return feedbacks[`${internProgress.style.backgroundColor}`];
+};
+
+const printFeedback = () => {
+  const container = document.getElementById("container");
+  const feedback = document.createElement("p");
+  feedback.id = "feedback";
+  feedback.textContent = handleFeedback();
+  container.append(feedback);
+};
+
 const handlePasswordInput = (event) => {
   const inputValue = event.target.value;
 
   const progressBar = document.getElementById("intern-progress");
   progressBar.style.width = increaseProgressBar(inputValue);
   progressBar.style.backgroundColor = handleProgressBarColor(inputValue);
+
+  printFeedback();
 };
 
 const password = document.getElementById("password");
