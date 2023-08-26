6ce5e4730876f81c0415967b71c5c4712155149c
unknown <marlon_tancredo@hotmail.com>
2023-08-26 20:11:24 +0100

    feat: implementing handleProgressBarColor function

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ const increaseProgressBar = (inputValue) => {
   return `${inputValue.length * 5}%`;
 };
 
+const handleProgressBarColor = (inputValue) => {
+  if (inputValue.length < 8) {
+    return "red";
+  } else if (inputValue.length >= 8 && inputValue.length < 12) {
+    return "orange";
+  } else if (inputValue.length >= 12) {
+    return "green";
+  }
+  return "black";
+};
+
 const handlePasswordInput = (event) => {
   const inputValue = event.target.value;
 
   const progressBar = document.getElementById("intern-progress");
   progressBar.style.width = increaseProgressBar(inputValue);
+  progressBar.style.backgroundColor = handleProgressBarColor(inputValue);
 };
 
 const password = document.getElementById("password");
