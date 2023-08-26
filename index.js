2ca039d11df16b6dd055b43b0882df3a9b546e57
unknown <marlon_tancredo@hotmail.com>
2023-08-26 20:10:09 +0100

    feat: implementing increaseProgressBar function

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+const increaseProgressBar = (inputValue) => {
+  if (inputValue.length > 20) {
+    return "100%";
+  }
+  return `${inputValue.length * 5}%`;
+};
+
 const handlePasswordInput = (event) => {
-  console.log(event.target.value);
+  const inputValue = event.target.value;
+
+  const progressBar = document.getElementById("intern-progress");
+  progressBar.style.width = increaseProgressBar(inputValue);
 };
 
 const password = document.getElementById("password");
-password.addEventListener("input", handleInput);
+password.addEventListener("input", handlePasswordInput);
